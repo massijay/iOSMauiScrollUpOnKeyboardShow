4a617f1d83172bce39fbfa74cc53f31686598dc6
Massimiliano Cristarella <m.cristarella@outlook.com>
2023-02-28 13:39:48 +0100

    Add Maui sample app

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace iOSMauiScrollUpOnKeyboardShow;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace iOSMauiScrollUpOnKeyboardShow;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
