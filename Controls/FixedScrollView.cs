dd04f737887cac466887b64f065e68071c853f75
Massimiliano Cristarella <m.cristarella@outlook.com>
2023-02-28 14:05:27 +0100

    Add FixedScrollView to avoid hiding the Entry on iOS

---
+++ Controls/FixedScrollView.cs
@@ -0,0 +1,41 @@
+﻿#if IOS
+using Microsoft.Maui.Platform;
+using iOSMauiScrollUpOnKeyboardShow.Platforms.iOS.Utils;
+using UIKit;
+using CoreGraphics;
+#endif
+
+namespace iOSMauiScrollUpOnKeyboardShow.Controls
+{
+    public class FixedScrollView : ScrollView
+    {
+        public FixedScrollView()
+        {
+#if IOS
+            UIKeyboard.Notifications.ObserveWillShow(OnKeyboardShowing);
+            UIKeyboard.Notifications.ObserveWillHide(OnKeyboardHiding);
+#endif
+        }
+
+#if IOS
+        private void OnKeyboardShowing(object sender, UIKeyboardEventArgs args)
+        {
+            if (Shell.Current.CurrentPage is ContentPage page)
+            {
+                UIView control = this.ToPlatform(Handler.MauiContext).FindFirstResponder();
+                UIView rootUiView = page.Content.ToPlatform(Handler.MauiContext);
+                CGRect kbFrame = UIKeyboard.FrameEndFromNotification(args.Notification);
+                double distance = control.GetOverlapDistance(rootUiView, kbFrame);
+                if (distance > 0)
+                {
+                    Margin = new Thickness(Margin.Left, -distance, Margin.Right, distance);
+                }
+            }
+        }
+        private void OnKeyboardHiding(object sender, UIKeyboardEventArgs args)
+        {
+            Margin = new Thickness(Margin.Left, 0, Margin.Right, 0);
+        }
+#endif
+    }
+}
